7209d8a5673b99b987d03fd0dd492dccd14b8463
HARSHHVAISHNAV <harshvaishnav6060@gmail.com>
2024-03-18 21:51:16 +0530

    twwo

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("title").innerHTML = "genZhub";
+document.getElementsByClassName("navone")[0].innerHTML = "Home";
+document.getElementsByClassName("navone")[1].innerHTML = "Technology";
+document.getElementsByClassName("navone")[2].innerHTML = "Social Media";
+document.getElementsByClassName("navone")[3].innerHTML = "Business";
+document.getElementsByClassName("navone")[4].innerHTML = "Sports";
+document.getElementsByClassName("navone")[5].innerHTML = "Entertainment";
