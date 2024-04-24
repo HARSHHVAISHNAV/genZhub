7b20de56bf8a048890d93215c615a08f817b5655
HARSHHVAISHNAV <harshvaishnav6060@gmail.com>
2024-04-24 16:15:56 +0530

    home page is ready

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-document.getElementById("title").innerHTML = "genZhub";
-document.getElementsByClassName("navone")[0].innerHTML = "Home";
-document.getElementsByClassName("navone")[1].innerHTML = "Technology";
-document.getElementsByClassName("navone")[2].innerHTML = "Social Media";
-document.getElementsByClassName("navone")[3].innerHTML = "Business";
-document.getElementsByClassName("navone")[4].innerHTML = "Sports";
-document.getElementsByClassName("navone")[5].innerHTML = "Entertainment";
-
 function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    if (dropdown.style.display === "block") {
-      dropdown.style.display = "none";
-    } else {
+  var dropdown = document.getElementById("myDropdown");
+  if (dropdown.style.display = "none") {
       dropdown.style.display = "block";
-    }
+  } else {
+      dropdown.style.display = "block";
   }
+}
+function toggleDropdownLeave() {
+  var dropdown = document.getElementById("myDropdown");
+      dropdown.style.display = "none";
+} 
